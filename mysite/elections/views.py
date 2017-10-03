f9ae5da61cd8af9878b6c230007d8ce0bacba1ac
kwonheejeong <gmlwjd9405@naver.com>
2017-10-03 23:50:41 +0900

    Fill templates with dynamic information

--- mysite/elections/views.py
+++ mysite/elections/views.py
@@ -7,6 +7,9 @@ from .models import Candidate
 def index(request):
     # Candidate로 저장된 모든 DB의 내용을 불러와 확인
     candidates = Candidate.objects.all()
+    # dictionary 구조로 DB에 있는 내용을 가져온다.
+    context = {'candidates' : candidates}
 
-    return render(request, 'elections/index.html')
+    # context로 html에 모든 후보에 대한 정보를 전달
+    return render(request, 'elections/index.html', context)
 
