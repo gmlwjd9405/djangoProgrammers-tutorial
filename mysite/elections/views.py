01012cd5a4dae391baaecbcde83ed27353defec7
kwonheejeong <gmlwjd9405@naver.com>
2017-10-03 21:47:03 +0900

    Add templates for elections apps

--- mysite/elections/views.py
+++ mysite/elections/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 from .models import Candidate
 
 # Create your views here.
 def index(request):
+    # Candidate로 저장된 모든 DB의 내용을 불러와 확인
     candidates = Candidate.objects.all()
-    str = ''
-    for candidate in candidates:
-        str += "<p>{} 기호{}번({})<br>".format(candidate.name, candidate.party_number, candidate.area)
-        str += candidate.introduction+"</p>"
 
-    return HttpResponse(str)
+    return render(request, 'elections/index.html')
 
